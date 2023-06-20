055940407fb5b3db73ad070c491641aeaabe7eb1
Michal Škoula <imichalthemichal@gmail.com>
2023-06-20 10:56:49 +0200

    Change input variable to object, fixed math mistake

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
+console.log('working');
 function isSpacebar() {
   if(event.key === 'Enter') {
     getResults()
-  }  
+  }
 }
+function values() { 
+  let inputs = {
+    gradeAvg: Number(document.querySelector('.grade-avg').value),
+    gradeTotalWeight: Number(document.querySelector('.grade-total-weight').value),
+    nextGradeWeight: Number(document.querySelector('.next-grade-weight').value),
+    targetGradePercentage: Number(document.querySelector('.target-grade-percentage').value),
+    results: document.querySelector('.result')
+  }
+  return inputs;
+}
+
 function getResults() {
-  
-  const gradeAvg = Number(document.querySelector('.grade-avg').value)
-  const gradeTotalWeight = Number(document.querySelector('.grade-total-weight').value)
-  const nextGradeWeight = Number(document.querySelector('.next-grade-weight').value)
-  const targetGradePercentage = Number(document.querySelector('.target-grade-percentage').value)
-  let results = document.querySelector('.result')
-  let result
+  inputs = values()
 
-  if(gradeAvg && gradeTotalWeight && nextGradeWeight && targetGradePercentage) {
+  if(inputs.gradeAvg && inputs.gradeTotalWeight && inputs.nextGradeWeight && inputs.targetGradePercentage) {
 
-    // console.log(`Grade avg: ${gradeAvg} \nGrade Total Weight: ${gradeTotalWeight}\nNext Grade Weight: ${nextGradeWeight}\nTarget Grade %: ${targetGradePercentage}`)
+    let result = 0;
 
     for(let i = 0; i <= 101; i++){
-      result = ((gradeAvg * gradeTotalWeight) + (i * nextGradeWeight)) / (Number(gradeTotalWeight)+Number(nextGradeWeight))        
+      result = ((inputs.gradeAvg * inputs.gradeTotalWeight) + (i * inputs.nextGradeWeight)) / (inputs.gradeTotalWeight + inputs.nextGradeWeight)
+      console.log(`i: ${i} result: ${result} true: ${result >= inputs.targetGradePercentage}`) 
 
-      if(result > targetGradePercentage) {
-        results.innerText = `You need at least ${i} to pass.`
+      if(result >= inputs.targetGradePercentage) {
+        inputs.results.innerText = `You need at least ${i} to pass.`
         break;
       } 
       else if(i === 101) {
-        results.innerText = `Yeah you\'re not fixing this man XD`
+        inputs.results.innerText = `I'm afraid you're a bit screwed XD`
         break;
       } 
     }
-  }
-  
+  }  
   else {
-    results.innerText = `Please fill out all of the fields.`
+    inputs.results.innerText = `Please fill out all of the fields.`
   }     
 }
+function gradePredicter() {
+
+}
+/*
+Switch from next text predictor / Grade fixer
+*/
