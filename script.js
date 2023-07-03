91e674bf6dd78db2c3f00dc4ea83e1673a2131a9
Michal Škoula <imichalthemichal@gmail.com>
2023-07-03 19:53:42 +0200

    Finish redesign, add grade fixer tool

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ function values() {
   return inputs;
 }
 
+function getAverage(gradeAvg = Number(), gradeTotalWeight = Number(), i = Number(), nextGradeWeight = Number()){
+  return ((gradeAvg * gradeTotalWeight + i * nextGradeWeight) / (gradeTotalWeight + nextGradeWeight))
+
+}
+
 function getResults() {
   inputs = values()
 
@@ -23,15 +28,17 @@ function getResults() {
     let result = 0;
 
     for(let i = 0; i <= 101; i++){
-      result = ((inputs.gradeAvg * inputs.gradeTotalWeight) + (i * inputs.nextGradeWeight)) / (inputs.gradeTotalWeight + inputs.nextGradeWeight)
-      console.log(`i: ${i} result: ${result} true: ${result >= inputs.targetGradePercentage}`) 
+      result = getAverage(inputs.gradeAvg, inputs.gradeTotalWeight, i, inputs.nextGradeWeight)
+      // console.log(`i: ${i} result: ${result} true: ${result >= inputs.targetGradePercentage}`) 
 
       if(result >= inputs.targetGradePercentage) {
         inputs.results.innerText = `You need at least ${i} to pass.`
+        return i
         break;
       } 
       else if(i === 101) {
-        inputs.results.innerText = `I'm afraid you're a bit screwed XD`
+        inputs.results.innerText = `I'm afraid you're a bit screwed 😆`
+        return -1
         break;
       } 
     }
@@ -40,9 +47,29 @@ function getResults() {
     inputs.results.innerText = `Please fill out all of the fields.`
   }     
 }
-function gradePredicter() {
+function gradeFixer() {
+  let inputs = values()
+  console.log(inputs)
+  
+  let predictions = []
+  let average = Number()
+  let maxWeight = 10
 
+  for (weight = 1; weight <= maxWeight; weight++) {
+    for(i = 0; i <= 101; i++) {
+      average = getAverage(inputs.gradeAvg, inputs.gradeTotalWeight, i, weight)
+
+      if(i == 101) {
+        predictions[weight - 1] = -1
+      }
+
+      else if(i <= 100 && average >= inputs.targetGradePercentage){
+        console.log(i)
+        predictions[weight - 1] = i
+        break;
+      }
+    }
+  }
+  inputs.results.innerText = `You will need at least: ${predictions} for weights 1-10 `
+  return predictions
 }
-/*
-Switch from next text predictor / Grade fixer
-*/
